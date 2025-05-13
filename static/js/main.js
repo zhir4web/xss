b447edd7e3b582d731cea1aee06b0632e4b063c0
ZhirDev <zherjalal123@gmail.com>
2025-05-13 23:19:48 +0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,9 @@
 console.log('This is JS from your About page.')
+fetch("/api/", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "X-CSRFToken": getCookie("csrftoken"),  // getCookie تابعەیە کە توکینەکە دەخواتەوە
+  },
+  body: JSON.stringify(data),
+});
